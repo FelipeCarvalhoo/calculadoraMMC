0b41cab70575b89b0e3521a8e3ce9db7be611f37
Felipe <fcarvalhoosilva@gmail.com>
2024-12-06 16:13:13 -0300

    adicionei algumas divs e aloquei as imagens dentro das divs

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,11 +1,23 @@
 import { levels } from "../../helpers/imc"; //escri levels e não level ok!
+import styles from './GridItem.module.css';
+import upImage from '../../assets/up.png';
+import downImage from '../../assets/down.png';
 
 type Props = { 
     item: levels
 };
+//ali na linha14 eu poderia fazer assim {item.icon === 'up' && <img src={upImage} alt="" width="30"/>} e {item.icon === 'down' && <img src={downImage} alt="" width="30"/>} this is economy
+
 
 export const GridItem = ({item}: Props) => {
     return (
-         <div>...</div>
+<div className={styles.main} style={{ backgroundColor: item.color }}>
+<div className={styles.gridIcon}>
+<img src={item.icon === 'up' ? upImage : downImage} alt="" width="30"/> 
+
+</div>
+<div className={styles.gridTitle}></div>
+<div className={styles.gridInfo}></div>
+</div>
 );
-}
+}  

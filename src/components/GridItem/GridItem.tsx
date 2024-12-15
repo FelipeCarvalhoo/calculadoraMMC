dbac0e454067e3b75da8cf71d1193b1a7b49d08c
Felipe <fcarvalhoosilva@gmail.com>
2024-12-15 15:27:07 -0300

    editei gridInfo entre outras coisas

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -16,8 +16,12 @@ export const GridItem = ({item}: Props) => {
 <img src={item.icon === 'up' ? upImage : downImage} alt="" width="30"/> 
 
 </div>
-<div className={styles.gridTitle}></div>
-<div className={styles.gridInfo}></div>
+<div className={styles.gridTitle}>{item.title}</div>
+<div className={styles.gridInfo}>
+<> 
+IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+</>
+</div>
 </div>
 );
 }  
